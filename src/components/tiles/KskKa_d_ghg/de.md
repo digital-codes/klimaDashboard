503249060fc6692c723cae7078b99b30fb43d62f
Andreas Kugel <devel@akugel.de>
2024-07-10 10:46:22 +0200

    update monitor

--- src/components/tiles/KskKa_d_ghg/de.md
+++ src/components/tiles/KskKa_d_ghg/de.md
@@ -1,7 +1,7 @@
-## Klimabilanz Karlsruhe
-
+## Energie Emissionen
+ 
 bla bla bla bla bla fmwölkwe
-ddd   ddd qdwdw  
+ddd   ddd qdwdw  wdwd
 [link](https://www.google.com)
 
   * 123
--- tools/mdMonitorPlugin.js
+++ tools/mdMonitorPlugin.js
@@ -13,6 +13,13 @@ const markdownDirs = [
   path.resolve(__dirname, '../src/components/tiles/dummyLineApx'),
   path.resolve(__dirname, '../src/components/tiles/dummyMap'),
   path.resolve(__dirname, '../src/components/tiles/dummyTable'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_consulting'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_electricity'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_energy'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_ghg'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_heat'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_mobility'),
+  path.resolve(__dirname, '../src/components/tiles/KskKa_d_pv'),
 ];
 
 // Define the bash script to execute
@@ -21,27 +28,34 @@ const bashScript = path.resolve(__dirname, './mdCompileAll.sh');
 const customMarkdownPlugin = () => ({
   name: 'custom-markdown-plugin',
   handleHotUpdate({ file, server }) {
-    if ((file.endsWith("card.json") || file.endsWith('.md')) && markdownDirs.some(dir => file.startsWith(dir))) {
-      // Execute the bash script
-      //exec(`${bashScript} ${dir} ${file}`, (error, stdout, stderr) => {
-        exec(`${bashScript} ${file}`, (error, stdout, stderr) => {
-          if (error) {
-          console.error(`Error executing script: ${error}`);
-          return;
-        }
-        if (stderr) {
-          console.error(`Script stderr: ${stderr}`);
-          return;
-        }
-        console.log(`Script stdout: ${stdout}`);
-      });
-
+    if ((file.endsWith("card.json") || file.endsWith("lang.json")) && markdownDirs.some(dir => file.startsWith(dir))) {
       // Reload the server
       server.ws.send({
         type: 'full-reload',
       });
+    } else {
+      if (file.endsWith('.md') && markdownDirs.some(dir => file.startsWith(dir))) {
+        // Execute the bash script
+        //exec(`${bashScript} ${dir} ${file}`, (error, stdout, stderr) => {
+        exec(`${bashScript} ${file}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error executing script: ${error}`);
+            return;
+          }
+          if (stderr) {
+            console.error(`Script stderr: ${stderr}`);
+            return;
+          }
+          console.log(`Script stdout: ${stdout}`);
+        });
+
+        // Reload the server
+        server.ws.send({
+          type: 'full-reload',
+        });
+      }
     }
-  },
+  }
 });
 
 export default customMarkdownPlugin;
